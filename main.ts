4e530eef3b7ad5cbd7cbb6360e90f881a3396419
Daniel Gallichan <dgallichan@yahoo.com>
2022-11-15 13:51:11 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -31,12 +31,12 @@ radio.onReceivedValueDeprecated(function (name, value) {
         if (LeftOutput > 0) {
             robobit.move(RBMotor.Left, RBDirection.Forward, topSpeed * LeftOutput)
         } else {
-            robobit.move(RBMotor.Left, RBDirection.Reverse, topSpeed * LeftOutput)
+            robobit.move(RBMotor.Left, RBDirection.Reverse, topSpeed * (-1 * LeftOutput))
         }
         if (RightOutput > 0) {
             robobit.move(RBMotor.Right, RBDirection.Forward, topSpeed * RightOutput)
         } else {
-            robobit.move(RBMotor.Right, RBDirection.Reverse, topSpeed * RightOutput)
+            robobit.move(RBMotor.Right, RBDirection.Reverse, topSpeed * (-1 * RightOutput))
         }
     }
 })
